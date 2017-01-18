1cc9523cae8d7078e7ec7a7713af4c2a98c8125e
calebomusic <calebomusic@gmail.com>
2017-01-18 14:19:18 -0800

    Init with webpack, react, redux environment

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './frontend/fetchTutorial.js',
+  output: {
+    filename: './bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
